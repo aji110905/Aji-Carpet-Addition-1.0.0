e767d426e9447aaee320c1bf90d6e105ad122e17
aji110905 <2442733838@qq,com>
2025-08-01 11:10:10 +0800

    Initial commit

---
+++ src/main/java/aji/aji_carpet_addition/AjiCarpetAdditionDataGenerator.java
@@ -0,0 +1,11 @@
+package aji.aji_carpet_addition;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AjiCarpetAdditionDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
